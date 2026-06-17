1491a6c6753a11afee74b9e97613c5bee04f6bf4
Hector Manuel <nathanielkango@gmail.com>
2026-06-17 16:20:14 +0000

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+require('dotenv').config();
+
+module.exports = {
+    WARN_COUNT: 3
+};
---
+++ settings.js
@@ -0,0 +1,21 @@
+const settings = {
+  packname: 'Queen Riam',
+  author: 'Hector Manuel',
+  botName: 'Queen Riam',
+  botOwner: 'Hector Manuel',
+  timezone: 'Africa/Accra',
+  prefix: '.',
+  ownerNumber: '',
+  AUTO_STATUS_REACT: 'true',
+  AUTO_STATUS_REPLY: 'false',
+  AUTO_STATUS_MSG: 'Status Viewed Queen Riam',
+  AUTORECORD: 'false',
+  AUTOTYPE: 'false',
+  AUTORECORDTYPE: 'false',
+  giphyApiKey: '',
+  commandMode: 'public',
+  description: 'This is a bot for managing group commands and automating tasks.',
+  version: '1.0.0',
+};
+
+module.exports = settings;
